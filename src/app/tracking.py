cb4534f37964a85d89ce475f87689c75776ded59
Will Meyers <williamgmeyers@gmail.com>
2016-09-06 09:40:20 -0400

    OOP

--- src/app/tracking.py
+++ src/app/tracking.py
@@ -2,62 +2,80 @@ import numpy as np
 import cv2
 
 
-class TrackingWindow:
-    def __init__(self, video_cap):
-        self.cap = cv2.VideoCapture(0)
-
-        self.feature_params = dict(
-            maxCorners = 200,
-            qualityLevel = 0.3,
-            minDistance = 7,
-            blockSize = 7
-        )
+class Tracker:
+    def __init__(self, video_stream):
+        self.vstream = video_stream
+        self.running = True
 
         self.lk_params = dict(
-            winSize = (15, 15),
+            winSize = (21, 21),
             maxLevel = 2,
             criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
         )
 
-        ret, old_frame = self.cap.read()
-        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-        p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **self.feature_params) 
+        self.init_frame = None
+        self.init_gray = None
+        self.p0 = None
 
-        mask = np.zeros_like(old_frame)
-
-        while True:
-            ret, frame = self.cap.read()
-            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    def capture_initial_frame(self):
+        _, self.init_frame = self.vstream.read()
+        self.init_gray = cv2.cvtColor(self.init_frame, cv2.COLOR_BGR2GRAY)
         
-            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **self.lk_params)
+    def process_frame(self, frame):
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        p1, st, err = cv2.calcOpticalFlowPyrLK(self.init_gray, frame_gray, self.p0, None, **self.lk_params)
 
+        if st == 1:
             good_new = p1[st==1]
-            good_old = p0[st==1]
+            good_old = self.p0[st==1]
 
             for i, (new, old) in enumerate(zip(good_new, good_old)):
                 a, b = new.ravel()
                 c, d = old.ravel()
 
-                cv2.line(frame, (a,b), (c,d), (255, 0, 255), 2)
                 cv2.circle(frame, (a,b), 5, (0, 255, 0), -1)
-            
-            #img = cv2.add(frame, mask)
+        
+        self.init_gray = frame_gray.copy()
+        if st==1:
+            self.p0 = good_new.reshape(-1, 1, 2)
 
-            cv2.imshow('frame', frame)
-            
-            if cv2.waitKey(30) & 0xff == ord('q'):
-                break
+        return frame
 
-            old_gray = frame_gray.copy()
-            p0 = good_new.reshape(-1, 1, 2)
+    def run(self):
+        self.capture_initial_frame()
+        self.set_point(120, 120)
+        while True:
+            _, f = self.vstream.read()
+            cv2.imshow('hey', self.process_frame(f))
+            cv2.waitKey(30)
 
-        cv2.destroyAllWindows()
-        self.cap.release()
+    def set_point(self, x, y):
+        self.p0 = [[(x, y)]]
+        self.p0 = np.float32(np.asarray(self.p0))
 
-    def set_point(self):
+t = Tracker(cv2.VideoCapture(0))
+t.run()
+
+
+class TrackerDisplay:
+    def __init__(self):
+        self.draw_states = {
+            'NONE': 0,
+            'LINE': 1,
+            'POINT': 2
+        }
+
+        self.selected_draw_state = self.draw_states['NONE']
+
+        self.mask = None
+        self.display_frame = None
+
+    def show_frame(self):
         pass
 
-    def show(self):
+    def draw_line(self):
         pass
 
-c = TrackingWindow(0)
+    def draw_point(self):
+        pass
