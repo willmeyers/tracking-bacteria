aa37fa724dd64e9886e39457eca9c2b22df20923
Will Meyers <willmeyers@users.noreply.github.com>
2016-09-10 15:29:09 -0400

    Delete protozoa.py

--- protozoa.py
+++
@@ -1,9 +0,0 @@
-class Protozoa: 
-    def __init__(self, identifier, rect):
-        self.identifier = identifier
-        self.prev_positions = []
-   
-        self.x = rect[0]
-        self.y = rect[1]
-        self.w = rect[2]
-        self.h = rect[3]
